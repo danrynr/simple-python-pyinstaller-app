85970734cfbdc22e6020b8f50436037cb1c7235f
danrynr <daniel.reynard.k@gmail.com>
2022-09-20 14:35:04 +0700

    bruh

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     install_requires=[
         'pyinstaller',
     ],
+    py_modules=[
+        'add2vals',
+    ],
     entry_points={
         'console_scripts': [
             'simple-python-pyinstaller-app = simple_python_pyinstaller_app.__main__:main',
