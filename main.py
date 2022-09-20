61a527bf0caeff8ab6d8f2ce73ea9611a57d570b
danrynr <daniel.reynard.k@gmail.com>
2022-09-20 16:02:57 +0700

    edit setup.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ use it to calculate 2 numbers
 """
 from sources import add2vals
 
-import flask from Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return add2vals.add2vals(20, 11)
+    return add2vals.add2(20, 11)
 
 if __name__ == '__main__':
     app.run()
